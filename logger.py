939390a6fb885ce4b163e7b34f5f25b668904631
Lucien-DM <lucien@nuggetcomputersinc.net>
2026-06-08 20:11:33 +1000

    modified:   logger.py -Fixed a bug where calling warn would return an error instead of a warn. -Fixed proper private method, variable, and object encapsulation.

--- logger.py
+++ logger.py
@@ -2,23 +2,22 @@
 import time
 
 LOG_FILE = "run.log"
-MODE = "disabled"
 
 class Logger:
     """Logger, outputs nothing when called. Parent class to other loggers"""
-    def message(self, level, msg):
+    def _message(self, level, msg):
         """Formulates message"""
         return f"[{time.time()}] [{level}] {msg}"
     def error(self, msg):
         """error"""
-        self.output(self.message("ERROR", msg))
+        self._output(self._message("ERROR", msg))
     def warn(self, msg):
         """warn"""
-        self.output(self.message("WARN", msg))
+        self._output(self._message("WARN", msg))
     def info(self, msg):
         """info"""
-        self.output(self.message("INFO", msg))
-    def output(self, msg):
+        self._output(self._message("INFO", msg))
+    def _output(self, msg):
         """pass"""
 
 class DummyLogger:
@@ -32,39 +31,39 @@ class DummyLogger:
 
 class TerminalLogger(Logger):
     """Logger that outputs to Terminal"""
-    def output(self, msg):
+    def _output(self, msg):
         """Output"""
         print(msg)
 
 class FileLogger(Logger):
     """Logger that outputs to file"""
-    def output(self, msg):
+    def _output(self, msg):
         with open(LOG_FILE, "a", encoding="utf-8") as file:
             file.write(f'{msg}\n')
 
 class FileTerminalLogger(Logger):
     """logger that outputs to both File and Terminal"""
-    def output(self, msg):
+    def _output(self, msg):
         with open(LOG_FILE, "a", encoding="utf-8") as file:
             file.write(f'{msg}\n')
         print(msg)
 
-log_class = DummyLogger()
+_log_class = DummyLogger()
 
 def mode_update(new_mode):
     """Updates the logger module mode"""
-    global log_class
+    global _log_class
     if new_mode == "disabled":
-        log_class = DummyLogger()
+        _log_class = DummyLogger()
 
     elif new_mode == "terminal":
-        log_class = TerminalLogger()
+        _log_class = TerminalLogger()
 
     elif new_mode == "file":
-        log_class = FileLogger()
+        _log_class = FileLogger()
 
     elif new_mode == "file_terminal":
-        log_class = FileTerminalLogger()
+        _log_class = FileTerminalLogger()
 
     else:
         raise ValueError(f"Valid Modes are 'disabled', 'terminal' 'file' and\
@@ -72,10 +71,10 @@ def mode_update(new_mode):
 
 def error(msg):
     """Allows program to call error through module"""
-    log_class.error(msg)
+    _log_class.error(msg)
 def warn(msg):
     """Allows program to call warn through module"""
-    log_class.error(msg)
+    _log_class.warn(msg)
 def info(msg):
     """Allows program to call info through module"""
-    log_class.info(msg)
+    _log_class.info(msg)
